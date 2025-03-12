99b45c941bc8643389e6f02ea8f476cfae838e5e
Sin@n2007 <ms.sagir@student.avans.nl>
2025-03-12 22:21:50 +0100

    Jouw beschrijving van de wijziging

---
+++ Secure2D.Tests/Environment2DRepositoryTests.cs.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Secure2D.Models;
+using Secure2D.Repositories;
+
+namespace Secure2D.Tests
+{
+    [TestClass]
+    public class Environment2DRepositoryTests
+    {
+        private Mock<IEnvironment2DRepository> _mockRepo;
+        private List<Environment2D> _mockData;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _mockRepo = new Mock<IEnvironment2DRepository>();
+            _mockData = new List<Environment2D>
+            {
+                new Environment2D { Id = Guid.NewGuid(), Name = "TestEnv1", MaxHeight = 100, MaxLength = 200 },
+                new Environment2D { Id = Guid.NewGuid(), Name = "TestEnv2", MaxHeight = 150, MaxLength = 250 }
+            };
+        }
+
+        [TestMethod]
+        public async Task GetAllAsync_ShouldReturnListOfEnvironments()
+        {
+            // Arrange
+            _mockRepo.Setup(repo => repo.GetAllAsync()).ReturnsAsync(_mockData);
+
+            // Act
+            var result = await _mockRepo.Object.GetAllAsync();
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(2, result.Count());
+        }
+
+        [TestMethod]
+        public async Task GetByIdAsync_ShouldReturnCorrectEnvironment()
+        {
+            // Arrange
+            var testId = _mockData[0].Id;
+            _mockRepo.Setup(repo => repo.GetByIdAsync(testId)).ReturnsAsync(_mockData[0]);
+
+            // Act
+            var result = await _mockRepo.Object.GetByIdAsync(testId);
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(testId, result.Id);
+        }
+
+        [TestMethod]
+        public async Task DeleteAsync_ShouldReturnTrueWhenEnvironmentIsDeleted()
+        {
+            // Arrange
+            var testId = _mockData[0].Id;
+            _mockRepo.Setup(repo => repo.DeleteAsync(testId)).ReturnsAsync(true);
+
+            // Act
+            var result = await _mockRepo.Object.DeleteAsync(testId);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+    }
+}
--- Secure2D.Tests/Test1.cs
+++ Secure2D.Tests/Test1.cs
@@ -1,11 +1,52 @@
-﻿namespace Secure2D.Tests
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Secure2D.Controllers;
+using Secure2D.Models;
+using Secure2D.Repositories;
+
+namespace Secure2D.Tests
 {
     [TestClass]
-    public sealed class Test1
+    public class Environment2DControllerTests
     {
-        [TestMethod]
-        public void TestMethod1()
+        private Mock<IEnvironment2DRepository> _mockRepo;
+        private Environment2DController _controller;
+        private List<Environment2D> _mockData;
+
+        [TestInitialize]
+        public void Setup()
         {
+            _mockRepo = new Mock<IEnvironment2DRepository>();
+            _controller = new Environment2DController(_mockRepo.Object);
+            _mockData = new List<Environment2D>
+            {
+                new Environment2D { Id = Guid.NewGuid(), Name = "TestEnv1", MaxHeight = 100, MaxLength = 200 },
+                new Environment2D { Id = Guid.NewGuid(), Name = "TestEnv2", MaxHeight = 150, MaxLength = 250 }
+            };
+        }
+
+        [TestMethod]
+        public async Task Post_AddNewEnvironment_ReturnsCreatedEnvironment()
+        {
+            // Arrange
+            var newEnvironment = new Environment2D { Id = Guid.NewGuid(), Name = "NewEnv", MaxHeight = 100, MaxLength = 200 };
+            _mockRepo.Setup(repo => repo.AddAsync(newEnvironment)).Returns(Task.CompletedTask);
+
+            // Act
+            var response = await _controller.Post(newEnvironment);
+
+            // Assert
+            Assert.IsInstanceOfType(response, typeof(CreatedAtActionResult));
+            var createdResult = response as CreatedAtActionResult;
+            Assert.IsNotNull(createdResult);
+            Assert.IsInstanceOfType(createdResult.Value, typeof(Environment2D));
+            var actualEnvironment = createdResult.Value as Environment2D;
+            Assert.IsNotNull(actualEnvironment);
+            Assert.AreEqual(newEnvironment.Id, actualEnvironment.Id);
         }
     }
 }
